451fbf17fe3782083b1df84f124ff25db1d2c68a
Lucas Moschen <lucas.machadomoschen@gmail.com>
2020-08-01 22:42:45 -0300

    Little changes to improve results, less time

--- scripts/create_last_database.py
+++ scripts/create_last_database.py
@@ -74,7 +74,7 @@ class User:
 
     def get_id_by_name(self, user_name):
         '''Given a user name return a user_id if it
-        exists, or create one if it does not;'''
+        exists, or create one if it does not+'''
 
         users = pd.read_csv(self.users_file, index_col='user_id')
         df_artist = users[users.artist_name == user_name]
@@ -86,7 +86,7 @@ class User:
             user_id = len(users) + 1
             with open(self.users_file, 'a') as f:
                 f.write(str(user_id) + ',' + user_name + '\n')
-        return user_id
+        return int(user_id)
 
     def get_user_info(self, user_name: str, limit: int = 20) -> dict: 
         '''Get a lot of user information from last.fm and build it in a dictionary'''
@@ -97,21 +97,23 @@ class User:
         website += self.network.api_key
         website += '&format=json'
         get_json = requests.get(website)
-        user_info_json = json.loads(get_json.content)['user']
+        try: 
+            user_info_json = json.loads(get_json.content)['user']
+        except KeyError: 
+            print(user_name + ': ' + json.loads(get_json.content)['message'])
+            return {}
+
         
         user = self.network.get_user(user_name)
         user_info = {}
-        user_info['id'] = self.get_id_by_name(user_name)
         user_info['name'] = user_name
-        user_info['subscriber'] = int(user.is_subscriber())
-        user_info['playcount'] = user.get_playcount()
-        user_info['registered_since'] = user.get_registered()
-        user_info['country'] = None
+        user_info['subscriber'] = int(user_info_json['subscriber'])
+        user_info['playcount'] = user_info_json['playcount']
+        user_info['registered_since'] = user_info_json['registered']['unixtime']
+        user_info['country'] = user_info_json['country']
         user_info['age'] = int(user_info_json['age'])
         user_info['playlists'] = user_info_json['playlists']
         user_info['gender'] = user_info_json['gender']
-        if user.get_country():
-            user_info['country'] = user.get_country().name
 
         tracks = Track(self.network)
         artists = Artist(self.network)
@@ -120,8 +122,11 @@ class User:
 
         user_info['loved_tracks'] = [tracks.get_id_by_name(loved.track.title, loved.track.artist.name) 
                                      for loved in user.get_loved_tracks()]
-        user_info['recent_tracks'] = [tracks.get_id_by_name(recent.track.title, recent.track.artist.name) 
-                                     for recent in user.get_recent_tracks()]
+        try: 
+            user_info['recent_tracks'] = [tracks.get_id_by_name(recent.track.title, recent.track.artist.name) 
+                                        for recent in user.get_recent_tracks()]
+        except pylast.WSError:
+            user_info['recent_tracks'] = None
         user_info['top_tracks'] = [(tracks.get_id_by_name(top.item.title, top.item.artist.name),top.weight) 
                                      for top in user.get_top_tracks(limit = limit)]
         user_info['top_tags'] = [(tags.get_id_by_name(top.item.name), top.weight) 
@@ -130,6 +135,11 @@ class User:
                                      for top in user.get_top_albums(limit = limit)]
         user_info['top_artists'] = [(artists.get_id_by_name(top.item.name), top.weight) 
                                      for top in user.get_top_artists(limit = limit)]
+        
+        tracks.write_to_csv()
+        artists.write_to_csv()
+        albums.write_to_csv()
+        tags.write_to_csv()
         
         return user_info
 
@@ -142,9 +152,10 @@ class Track:
         if not os.path.exists(filepath):
             try: 
                 with open(filepath, 'w') as f:
-                    f.write('track_id,artist_name,track_name\n')
+                    f.write('track_id\tartist_name\ttrack_name\n')
             except: 
                 raise Exception('Problem in creating file. Maby the folder does not exist.')
+        self.tracks_df = pd.read_csv(self.tracks_file, index_col='track_id', sep = '\t')
             
     def _set_to_date(self, date: str) -> str:
         '''Given [day] [month] [year] [hour] pattern, return year-mm-dd'''
@@ -155,21 +166,24 @@ class Track:
                  'Sep':'07', 'Oct':'10', 'Nov':'11', 'Dec':'12' }[month]
         return year + '-' + month + '-' + day
 
+    def write_to_csv(self):
+
+        self.tracks_df.to_csv(self.tracks_file, sep = '\t')
+
     def get_id_by_name(self, track_name: str, artist_name: str) -> int: 
         '''Given an artist name and track name, return a track id if it
         exists, or create one if it does not'''
 
-        tracks = pd.read_csv(self.tracks_file, index_col='track_id')
-        df_track = tracks[(tracks.artist_name == artist_name) & (tracks.track_name == track_name)]
+        df_track = self.tracks_df[(self.tracks_df.artist_name == artist_name) 
+                                & (self.tracks_df.track_name == track_name)]
         if len(df_track) == 1:
             track_id = df_track.index[0]
         elif len(df_track) > 1:
             raise Exception('Two tracks with same artist name and same track name')
         else: 
-            track_id = len(tracks) + 1
-            with open(self.tracks_file, 'a') as f:
-                f.write(str(track_id) + ',' + artist_name + ',' + track_name + '\n')
-        return track_id
+            track_id = len(self.tracks_df) + 1
+            self.tracks_df.loc[track_id] = [artist_name, track_name]
+        return int(track_id)
 
     def get_track_info(self, track_name: str, artist_name: str, limit: int = 20) -> dict:
         '''Given an artist name and track name, get track info'''
@@ -185,7 +199,7 @@ class Track:
         track_info['published'] = self._set_to_date(track.get_wiki_published_date())
 
         tags = Tag(self.network)
-        track_info['toptags'] = [(tags.get_id_by_name(tag.item.name), tag.weight) 
+        track_info['top_tags'] = [(tags.get_id_by_name(tag.item.name), tag.weight) 
                                   for tag in track.get_top_tags(limit = limit)]
         track_info['similar'] = [(similar.item.title, 
                                   similar.item.artist.name, 
@@ -202,9 +216,14 @@ class Artist:
         if not os.path.exists(filepath):
             try: 
                 with open(filepath, 'w') as f:
-                    f.write('artist_id,artist_name\n')
+                    f.write('artist_id\tartist_name\n')
             except: 
                 raise Exception('Problem in creating file. Maby the folder does not exist.')
+        self.artists_df = pd.read_csv(self.artists_file, index_col='artist_id', sep = '\t')
+        
+    def write_to_csv(self):
+
+        self.artists_df.to_csv(self.artists_file, sep = '\t')
             
     def _set_to_date(self, date: str) -> str:
         '''Given [day] [month] [year] [hour] pattern, return year-mm-dd'''
@@ -217,19 +236,17 @@ class Artist:
 
     def get_id_by_name(self, artist_name):
         '''Given an artist name return a artist_id if it
-        exists, or create one if it does not;'''
+        exists, or create one if it does not+'''
 
-        artists = pd.read_csv(self.artists_file, index_col='artist_id')
-        df_artist = artists[artists.artist_name == artist_name]
+        df_artist = self.artists_df[self.artists_df.artist_name == artist_name]
         if len(df_artist) == 1:
-            artist_id = artist_id.index[0]
+            artist_id = df_artist.index[0]
         elif len(df_artist) > 1:
-            raise Exception('Two tracks with same artist name and same track name')
+            raise Exception('Two artists with same name')
         else: 
-            artist_id = len(artists) + 1
-            with open(self.artists_file, 'a') as f:
-                f.write(str(artist_id) + ',' + artist_name + '\n')
-        return artist_id
+            artist_id = len(self.artists_df) + 1
+            self.artists_df.loc[artist_id] = [artist_name]
+        return int(artist_id)
 
     def get_artist_info(self, artist_name: str, limit: int = 20) -> dict:
         '''Given an artist name, get artist info'''
@@ -265,9 +282,14 @@ class Album:
         if not os.path.exists(filepath):
             try: 
                 with open(filepath, 'w') as f:
-                    f.write('album_id,artist_name,album_name\n')
+                    f.write('album_id\tartist_name\talbum_name\n')
             except: 
                 raise Exception('Problem in creating file. Maby the folder does not exist.')
+        self.albums_df = pd.read_csv(self.albums_file, index_col='album_id', sep = '\t')
+        
+    def write_to_csv(self):
+
+        self.albums_df.to_csv(self.albums_file, sep = '\t')
             
     def _set_to_date(self, date: str) -> str:
         '''Given [day] [month] [year] [hour] pattern, return year-mm-dd'''
@@ -280,19 +302,18 @@ class Album:
             
     def get_id_by_name(self, album_name, artist_name):
         '''Given an artist name and album name, return a album id if it
-        exists, or create one if it does not;'''
+        exists, or create one if it does not+'''
 
-        albums = pd.read_csv(self.albums_file, index_col='album_id')
-        df_album = albums[(albums.artist_name == artist_name) & (albums.album_name == album_name)]
+        df_album = self.albums_df[(self.albums_df.artist_name == artist_name) 
+                            & (self.albums_df.album_name == album_name)]
         if len(df_album) == 1:
             album_id = df_album.index[0]
         elif len(df_album) > 1:
-            raise Exception('Two tracks with same artist name and same track name')
+            raise Exception('Two albums with same artist name and same album name')
         else: 
-            album_id = len(albums) + 1
-            with open(self.albums_file, 'a') as f:
-                f.write(str(album_id) + ',' + artist_name + ',' + album_name + '\n')
-        return album_id
+            album_id = len(self.albums_df) + 1
+            self.albums_df.loc[album_id] = [artist_name, album_name]
+        return int(album_id)
 
     def get_album_info(self, album_name: str, artist_name: str, limit: int = 20) -> dict:
         '''Given an artist name and album name, get album info'''
@@ -325,6 +346,11 @@ class Tag:
                     f.write('tag_id,tag\n')
             except: 
                 raise Exception('Problem in creating file. Maby the folder does not exist.')
+        self.tags_df = pd.read_csv(self.tags_file, index_col='tag_id', sep = '\t')
+
+    def write_to_csv(self):
+
+        self.tags_df.to_csv(self.tags_file, sep = '\t')
             
     def _set_to_date(self, date: str) -> str:
         '''Given [day] [month] [year] [hour] pattern, return year-mm-dd'''
@@ -335,21 +361,19 @@ class Tag:
                  'Sep':'07', 'Oct':'10', 'Nov':'11', 'Dec':'12' }[month]
         return year + '-' + month + '-' + day
             
-    def get_id_by_name(self, tag,):
+    def get_id_by_name(self, tag):
         '''Given a tag, return a tag id if it
-        exists, or create one if it does not;'''
+        exists, or create one if it does not+'''
 
-        tags = pd.read_csv(self.tags_file, index_col='tag_id')
-        df_tag = tags[tags.tag == tag]
+        df_tag = self.tags_df[self.tags_df.tag == tag]
         if len(df_tag) == 1:
             tag_id = df_tag.index[0]
         elif len(df_tag) > 1:
             raise Exception('Two tracks with same artist name and same track name')
         else: 
-            tag_id = len(tags) + 1
-            with open(self.tags_file, 'a') as f:
-                f.write(str(tag_id) + ',' + tag + '\n')
-        return tag_id
+            tag_id = len(self.tags_df) + 1
+            self.tags_df.loc[tag_id] = [tag]
+        return int(tag_id)
 
     def get_tag_info(self, tag_name: str, limit: int = 20) -> dict:
         '''Given an artist name and tag name, get tag info'''
